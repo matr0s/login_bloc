f4ebf73ec8c927fcee778aa8b44848d9c3dede5c
matr0s <serhiy.osadchuk@gmail.com>
2021-02-09 22:49:18 +0200

    Some comments and tuning

--- lib/src/blocs/provider.dart
+++ lib/src/blocs/provider.dart
@@ -4,11 +4,19 @@ import 'bloc.dart';
 
 class Provider extends InheritedWidget {
   final bloc = Bloc();
+
+// **************************************
+// * We need this class constructor to be able to wrap another Widget as a Child
   Provider({Key key, Widget child}) : super(key: key, child: child);
 
   @override
+// **************************************
+// * It is required in case we extendet from InheritedWidget
   bool updateShouldNotify(_) => true;
 
+// **************************************
+// * This strange function is looking for the Prvider higer in the widget hierarchy
+// * starting the context of Widget we mentioned Provider.of(context) 
   static Bloc of(BuildContext context) {
     return (context.dependOnInheritedWidgetOfExactType<Provider>()).bloc;
   }
